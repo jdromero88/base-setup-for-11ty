8f37a915a85fc20bc9c6f289caa4a2b1ec9ab507
jdromero88 <jdromerov88@gmail.com>
2021-03-05 00:14:16 -0500

    Added some basic js config

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,12 @@
 let test = 'hola mundo'
 function start() {
 	console.log(test)
+	showCurrentYear()
+}
+
+function showCurrentYear() {
+	let currentYear = new Date()
+	let timeEl = document.getElementById('currentYear')
+	timeEl.textContent = currentYear.getFullYear()
 }
 document.addEventListener('DOMContentLoaded', start)
